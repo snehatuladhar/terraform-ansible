a89abad262a1a9dde40e039882177a43c9299244
snehatuladhar <snehatuladhar098@gmail.com>
2024-07-09 12:09:36 +0545

    workflow created

--- ec2.tf
+++ ec2.tf
@@ -9,6 +9,39 @@ module "ec2" {
   instance                    = var.instance
   ami_id                      = var.ami_id
 }
+
+# Creating a security group to restrict/allow inbound connectivity
+resource "aws_security_group" "network-security-group" {
+  name        = var.network_security_group_name
+  description = "Allow TLS inbound traffic"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+    ingress {
+    description = "Node App Port"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+  }
+  egress{
+    description ="SSM"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  
+  tags = {
+    Name = "nsg1"
+  }
+}
 resource "null_resource" "ansible" {
   depends_on = [module.ec2]
   provisioner "local-exec" {
@@ -17,3 +50,4 @@ resource "null_resource" "ansible" {
   EOT
   }
 }
+
